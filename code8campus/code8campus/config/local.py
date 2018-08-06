56f5216e149bdc144ad073b300d3868313ac76b9
Yoonje Choi <yoonjechoi@gmail.com>
2018-08-06 13:13:50 +0900

    fix(settings): fix base_dir and sqlite path

--- code8campus/code8campus/config/local.py
+++ code8campus/code8campus/config/local.py
@@ -19,6 +19,6 @@ INTERNAL_IPS = ('127.0.0.1',)
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(os.path.dirname(BASE_DIR), 'db.sqlite3'),
     }
 }
